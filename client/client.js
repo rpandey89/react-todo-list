77d6c9fc3f7654f9981b78f0b4b65aaeff7b835f
rpandey <rahul.pandey@insidesales.com>
2016-04-26 15:01:41 -0700

    Video 3 Changes

--- client/client.js
+++ client/client.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App'
 
+
+render(
+	React.createElement(App),
+	document.getElementById('app')
+);
+
+//React dev tool link
+  //http://facebook.github.io/react/blog/2015/09/02/new-react-developer-tools.html
+
+//'inline-source-map', better for dev env not for prod, we will see error in js that it corresponds to
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,22 @@
-
+module.exports = {
+	devtool : 'inline-source-map',
+	entry : ['./client/client.js'],
+	output : {
+		path : './dist',
+		filename : 'bundle.js',
+		publicPath : '/'
+	},
+	module : {
+		loaders : [
+			{
+				test : /\.js$/,
+				loader : 'babel-loader',
+				exclude : /node-modules/,
+				//set up .bablerc file and configure it or
+				query : {
+					presets : ['react', 'es2015']
+				}
+			}
+		]
+	}
+}
