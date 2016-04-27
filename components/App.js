ba44c221bba29a07b8206b861e97de11e40ff78f
rpandey <rahul.pandey@insidesales.com>
2016-04-26 18:54:55 -0700

    Video 4 changes

--- components/App.js
+++ components/App.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class App extends Component {
 
   render() {
-    return <div>This is definitely a React app now!</div>
+    return <div>This is definitely a React app now with hot module reloading blah blah!</div>
   }
 
 }
-
+//hot module reloading is only for dev environment for testing your changes
 export default App
--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var path = require('path');
+var config = require('../webpack.config.js');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+
+var compiler = webpack(config);
 
 var app = express();
+
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static('./dist'));
 
